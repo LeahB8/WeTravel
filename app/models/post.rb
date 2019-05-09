f7d5029c5377ed0d2cc8c8f986ca5fe1766d0c64
hlbtyne <harriet.ballantyne@gmail.com>
2019-05-09 12:38:00 +0100

    migration/model for post_tags, accosiations for new model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-  has_many :tags
+  has_many :posttags
+  has_many :posts, through: :post_tags
   has_many :comments
   belongs_to :user
   belongs_to :destination
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_08_090318) do
+ActiveRecord::Schema.define(version: 2019_05_09_113031) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 2019_05_08_090318) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
   end
 
   create_table "posts", force: :cascade do |t|
