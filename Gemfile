446e4587938883f5510f22907c7ecb81b36f0436
pmhofmann <pm.hofmann@yahoo.co.uk>
2019-05-10 09:16:21 +0100

    fake pokemon users

--- Gemfile
+++ Gemfile
@@ -65,6 +65,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bcrypt'
 
+gem 'faker'
 
 gem 'city-state'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@ Post.destroy_all
 leah = User.create(username: "LeahB", email: "leahb@gmail.com", password: "Hello!")
 harriet = User.create(username: "Harry", email: "harry@gmail.com", password: "Password")
 patrick = User.create(username: "Paddy", email: "paddy@gmail.com", password: "Goodbye")
+
+100.times do User.create(username: Faker::Games::Pokemon.unique.name, email: Faker::Internet.unique.email, password: "pokemon123")
+end
 #
 # london = Destination.create(location: "London")
 # majorca = Destination.create(location: "Majorca")
@@ -23,6 +26,9 @@ p3 = Post.create(user: patrick, title: "Lovely Trip", content: "It was a lovely 
 p4 = Post.create(user: leah, title: "Sangri-gri", content: "We drank lots of Sangria!", destination_id: 23 )
 p5 = Post.create(user: harriet, title: "Cultural Hub", content: "What a great city!", destination_id: 59 )
 p6 = Post.create(user: patrick, title: "Sweet locaysh", content: "Amazing!", destination_id: 211 )
+
+# 100.times do Post.create(user: User.all.sample, title: Faker::Lorem.sentence(2), content: Faker::Lorem.sentences(2), destination_id: Destination.all.id.sample)
+# end
 
 t1 = Tag.create(name: "Beach")
 t2 = Tag.create(name: "Sunset")
